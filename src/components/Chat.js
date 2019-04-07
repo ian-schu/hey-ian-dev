4ad9f7a49b57a02751d7d29f84513ce39437cbdf
Ian Schumann <ian.l.schumann@gmail.com>
2019-04-07 10:14:55 -0500

    Internalize chat styles

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,33 +1,56 @@
 import React from 'react'
-import styles from './Chat.module.css'
+import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 const Chat = props => {
   const chatData = props.data.chatBlob
   const authors = props.data.authors
 
-  return chatData.map(entry => ChatEntry(entry, authors))
+  return chatData.map((entry, idx) => (
+    <ChatEntry key={idx} entry={entry} authors={authors} />
+  ))
 }
 
 export default Chat
 
-const ChatEntry = (entry, authors) => {
+const ChatEntry = props => {
+  const { entry, authors } = props
   const author = authors.find(a => a.name === entry.author)
 
   return (
-    <div className={styles.entry}>
-      <div className={styles.portraitColumn}>
-        <Img fluid={author.avatar.fluid} className={styles.portrait} />
-      </div>
-      <div className={styles.entriesColumn}>
+    <Entry>
+      <Avatar>
+        <Img fluid={author.avatar.fluid} />
+      </Avatar>
+      <div>
         <div>
-          <span className={styles.author}>{entry.author}</span>
-          <span className={styles.timestamp}>4:35</span>
+          <Author>{entry.author}</Author>
+          <Timestamp>4:35</Timestamp>
         </div>
         {entry.text.map((text, idx) => (
           <div key={idx}>{text}</div>
         ))}
       </div>
-    </div>
+    </Entry>
   )
 }
+
+const Entry = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+`
+const Avatar = styled.div`
+  width: 75px;
+  img {
+    width: 75px;
+    height: 75px;
+    border: 1px solid gray;
+  }
+`
+const Author = styled.span`
+  font-weight: bold;
+`
+
+const Timestamp = styled.span`
+  color: gray;
+`
