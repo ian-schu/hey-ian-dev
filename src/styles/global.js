2bae20c3db6d5f0c1e1f11bc330e2c4ba072249c
Ian Schumann <ian.l.schumann@gmail.com>
2019-04-06 20:56:58 -0500

    Clear out global styles for Typography

--- src/styles/global.js
+++ src/styles/global.js
@@ -21,8 +21,6 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
-    font: inherit;
     vertical-align: baseline;
   }
 
@@ -84,12 +82,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     background: white;
     line-height: 1;
-    font-size: 100%;
-    font-variant-ligatures: none;
+    font-variant-ligatures: normal;
     text-rendering: optimizeLegibility;
     text-shadow: rgba(0, 0, 0, .01) 0 0 1px;
-    font-weight: 400;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   }
 
   img {
