b2b418f409bb073bf3b7b01088a5986263943c8c
Ian Schumann <ian.l.schumann@gmail.com>
2019-04-13 12:40:32 -0500

    Settle on Kirkham theme, with tweaks

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,16 @@
 import Typography from 'typography'
-import altonTheme from 'typography-theme-alton'
+import kirkhamTheme from 'typography-theme-kirkham'
 
-altonTheme.overrideStyles = ({ rhythm }, options) => ({
+kirkhamTheme.scaleRatio = 2.8
+kirkhamTheme.baseFontSize = '20px'
+kirkhamTheme.includeNormalize = true
+
+kirkhamTheme.overrideStyles = ({ rhythm }, options) => ({
   li: {
     marginBottom: '10px',
   },
 })
 
-const typography = new Typography(altonTheme)
+const typography = new Typography(kirkhamTheme)
 
 export default typography
